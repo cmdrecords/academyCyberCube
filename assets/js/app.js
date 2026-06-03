568c511913eb4c3c7a2f9382e29a10d87f32ab70
Kebablk <glbuhmaterik2010@gmail.com>
2026-06-03 18:22:21 +0300

    create

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,26 @@
-let x = "kebabis";
+const formInputs = document.querySelectorAll(".input-container_input");
+const nameIcon = document.querySelector(".name-icon");
+const phoneIcon = document.querySelector(".phone-icon");
+
+function changeImage(img, newSrc) {
+  img.style.opacity = "0";
+
+  setTimeout(() => {
+    img.src = newSrc;
+    img.style.opacity = "1";
+  }, 200);
+}
+
+formInputs[0].addEventListener("focus", () => {
+  changeImage(nameIcon, "assets/images/name-active.png");
+});
+formInputs[0].addEventListener("blur", () => {
+  changeImage(nameIcon, "assets/images/name-unactive.png");
+});
+
+formInputs[1].addEventListener("focus", () => {
+  changeImage(phoneIcon, "assets/images/phone-active.png");
+});
+formInputs[1].addEventListener("blur", () => {
+  changeImage(phoneIcon, "assets/images/phone-unactive.png");
+});
