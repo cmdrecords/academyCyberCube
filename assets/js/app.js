abce2419716e2345f85846fc067444a8cc8967a1
Kebablk <glbuhmaterik2010@gmail.com>
2026-06-03 15:51:34 +0300

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
-let x = "kebabis";
+var map = L.map("map", {
+  attributionControl: false,
+}).setView([45.1121, 38.9616], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+}).addTo(map);
+
+L.marker([45.1121, 38.9616])
+  .addTo(map)
+  .bindPopup("ул. им. Мурата Ахеджака, 10А<br>Краснодар")
+  .openPopup();
