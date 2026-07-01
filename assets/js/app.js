68544e1cd1af7fc79c4e72f9c02f461f82b2cc3d
Kebablk <glbuhmaterik2010@gmail.com>
2026-07-01 17:12:16 +0300

    add technologies line

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,35 @@
-let x = "kebabis";
+const lineIcons = [
+  "assets/images/icons8-c-sharp-logo-100.png",
+  "assets/images/icons8-c++-100.png",
+  "assets/images/icons8-css-100.png",
+  "assets/images/icons8-figma-100.png",
+  "assets/images/icons8-html-100.png",
+  "assets/images/icons8-javascript-100.png",
+  "assets/images/icons8-microsoft-office-2019-100.png",
+  "assets/images/icons8-nodejs-100.png",
+  "assets/images/icons8-photoshop-100.png",
+  "assets/images/icons8-python-100.png",
+  "assets/images/icons8-react-100.png",
+  "assets/images/icons8-sass-100.png",
+  "assets/images/icons8-scratch-100.png",
+  "assets/images/icons8-sublime-text-100.png",
+  "assets/images/icons8-unity-100.png",
+  "assets/images/icons8-unreal-engine-100.png",
+  "assets/images/icons8-vs-code-100.png",
+];
+
+const track = document.getElementById("sliderTrack");
+
+function createSlides(icons) {
+  let slidesHTML = "";
+  const doubledIcons = [...icons, ...icons];
+  doubledIcons.forEach((src) => {
+    slidesHTML += `
+                            <div class="slide">
+                                <img src="${src}" alt="Иконка" loading="lazy" />
+                            </div>
+                        `;
+  });
+  return slidesHTML;
+}
+track.innerHTML = createSlides(lineIcons);
